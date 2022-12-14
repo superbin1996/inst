5977915ae14aafaa234eebe194b79acb912c6f00
superbin1996 <superbin1996@gmail.com>
2022-12-14 20:01:20 +0900

    move media folder to instagram app

--- server/settings.py
+++ server/settings.py
@@ -194,7 +194,7 @@ STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 MEDIA_URL = 'media/'
 
 # Folder stores uploaded files
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'instagram/media/')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
